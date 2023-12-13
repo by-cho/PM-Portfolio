53dcfb72d824cf705bad164b1c3d94d06bd3d661
by-cho <149221715+by-cho@users.noreply.github.com>
2023-12-13 15:24:08 -0800

    home

---
+++ portfolio/pages/index.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Hero from '@/components/Hero'
+
+export default function Home() {
+  return (
+    <main>
+      <Hero></Hero>
+    </main>
+  )
+}
