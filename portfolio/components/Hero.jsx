0582825988bc6fb0521b442990a059bcbd05d863
by-cho <149221715+by-cho@users.noreply.github.com>
2023-12-15 15:25:00 -0800

    Update Hero.jsx

--- portfolio/components/Hero.jsx
+++ portfolio/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className='flex items-center justify-center h-screen mb-12 bg-center bg-cover custom-img'>
         <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]'/>
         <div className='p-5 z-[2] ml-[-10rem] mt-[-10rem]'>
-            <h2 className='h2'>Heading</h2>
+            <h2 className='h2'>Heading Text</h2>
             <p className='desc1'>Massage</p>
             <button className='button1'>Contact Me</button>
         </div>
